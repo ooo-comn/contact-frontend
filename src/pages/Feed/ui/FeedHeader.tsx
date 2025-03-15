98d7bf3d8c8543cd3466fedb6c42379b2c02c49d
KoUD2 <kostik-udod@mail.ru>
2025-03-15 14:12:26 +0300

    add index js

--- src/pages/Feed/ui/FeedHeader.tsx
+++ src/pages/Feed/ui/FeedHeader.tsx
@@ -1,5 +1,4 @@
 import { Link } from 'react-router-dom'
-import CreateCourseIcon from '../../../shared/assets/course/CreateCourse.svg'
 import styles from '../Feed.module.css'
 
 const FeedHeader = () => {
@@ -7,13 +6,7 @@ const FeedHeader = () => {
 		<div className={styles['feed__header']}>
 			<h1 className={styles['feed__title']}>Курсы</h1>
 			<Link to='connect-bot'>
-				<button className={styles['feed__create-button']}>
-					<img
-						src={CreateCourseIcon}
-						alt='Создать курс'
-						className={styles['feed__create-button-img']}
-					/>
-				</button>
+				<button className={styles['feed__create-button']}>+</button>
 			</Link>
 		</div>
 	)
