845357a1d08bc0796e2ec623b817c84b049dc24c
KoUD2 <kostik-udod@mail.ru>
2025-03-18 14:40:28 +0300

    update api

--- src/shared/config/api.ts
+++ src/shared/config/api.ts
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'https://comncourse.ru/api'
-export const BASE_URL = 'comncourse'
+export const API_BASE_URL = 'https://comncoursetest.ru/api'
+export const BASE_URL = 'comncoursetest'
