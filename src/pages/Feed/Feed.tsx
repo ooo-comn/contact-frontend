43deb5ac78d8baa40b5b6201d859376efac28df0
KoUD2 <kostik-udod@mail.ru>
2025-05-05 20:00:07 +0300

    rewrite register function

--- src/pages/Feed/Feed.tsx
+++ src/pages/Feed/Feed.tsx
@@ -1,55 +1,55 @@
-import { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
-import useUserCoursesData from '../../entities/user/model/useUserCourses'
-import NavBar from '../../shared/components/NavBar/NavBar'
-import styles from './Feed.module.css'
-import FeedFilters from './ui/FeedFilters'
-import FeedHeader from './ui/FeedHeader'
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import NavBar from "../../shared/components/NavBar/NavBar";
+import styles from "./Feed.module.css";
+import FeedFilters from "./ui/FeedFilters";
+import FeedHeader from "./ui/FeedHeader";
+import useUserContactsData from "src/entities/user/model/useUserContacts";
 
 const Feed = () => {
-	var BackButton = window.Telegram.WebApp.BackButton
+  var BackButton = window.Telegram.WebApp.BackButton;
 
-	if (BackButton.isVisible) {
-		BackButton.hide()
-	}
+  if (BackButton.isVisible) {
+    BackButton.hide();
+  }
 
-	const navigate = useNavigate()
-	const [activeFilter, setActiveFilter] = useState('Все контакты')
+  const navigate = useNavigate();
+  const [activeFilter, setActiveFilter] = useState("Все контакты");
 
-	const userId = window.Telegram.WebApp.initDataUnsafe.user.id
-	const { userCourses } = useUserCoursesData(userId, navigate)
+  const userId = window.Telegram.WebApp.initDataUnsafe.user.id;
+  const { userContacts } = useUserContactsData(userId, navigate);
 
-	// const {
-	// 	inputValue,
-	// 	setInputValue,
-	// 	filteredData,
-	// 	isPending,
-	// 	startTransition,
-	// } = useFeed(activeFilter, userCourses)
+  // const {
+  // 	inputValue,
+  // 	setInputValue,
+  // 	filteredData,
+  // 	isPending,
+  // 	startTransition,
+  // } = useFeed(activeFilter, userCourses)
 
-	console.log('userCoursesFeed', userCourses)
-	console.log(activeFilter)
+  console.log("userCoursesFeed", userContacts);
+  console.log(activeFilter);
 
-	return (
-		<div className={styles['feed']}>
-			<FeedHeader />
-			{/* <FeedSearch
+  return (
+    <div className={styles["feed"]}>
+      <FeedHeader />
+      {/* <FeedSearch
 				inputValue={inputValue}
 				onChange={e => startTransition(() => setInputValue(e.target.value))}
 			/> */}
-			<FeedFilters onFilterChange={setActiveFilter} />
-			{/* <FeedList filteredCourses={filteredData} isPending={isPending} /> */}
-			<Link
-				to='/subscription'
-				className={styles['feed__link-create-subscription']}
-			>
-				<button className={styles['feed__create-subscription']}>
-					Оформить подписку
-				</button>
-			</Link>
-			<NavBar />
-		</div>
-	)
-}
+      <FeedFilters onFilterChange={setActiveFilter} />
+      {/* <FeedList filteredCourses={filteredData} isPending={isPending} /> */}
+      <Link
+        to="/subscription"
+        className={styles["feed__link-create-subscription"]}
+      >
+        <button className={styles["feed__create-subscription"]}>
+          Оформить подписку
+        </button>
+      </Link>
+      <NavBar />
+    </div>
+  );
+};
 
-export default Feed
+export default Feed;
