27609f0cc6b2e18d1cfdf8df06b77e3ff4d241ea
KoUD2 <kostik-udod@mail.ru>
2025-05-08 10:29:48 +0300

    Comment out API_BASE_URL import in fetchUpdateUser to prepare for further refactoring and adjustments in API handling.

--- src/entities/user/model/fetchUpdateUser.ts
+++ src/entities/user/model/fetchUpdateUser.ts
@@ -1,4 +1,4 @@
-import { API_BASE_URL } from "../../../shared/config/api";
+// import { API_BASE_URL } from "../../../shared/config/api";
 
 // Функция для получения user_id по telegram_id через прокси
 const getUserIdByTelegramId = async (
