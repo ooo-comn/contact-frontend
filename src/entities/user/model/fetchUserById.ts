63497284a94abdc627b8de443fb31c39403a6961
KoUD2 <kostik-udod@mail.ru>
2025-05-30 17:42:01 +0300

    Refactor fetchUserById function to improve error handling and response structure; update API endpoint and request method for better data retrieval, ensuring consistent response format and enhanced debugging capabilities.

--- src/entities/user/model/fetchUserById.ts
+++ src/entities/user/model/fetchUserById.ts
@@ -1,22 +1,23 @@
-import { ITelegramUser } from 'src/entities/course/model/types'
-import { BASE_URL } from '../../../shared/config/api'
+import { ITelegramUser } from "src/entities/course/model/types";
+import { API_BASE_URL } from "../../../shared/config/api";
 
 export const fetchUserById = async (userId: number): Promise<ITelegramUser> => {
-	const response = await fetch(
-		`https://${BASE_URL}.ru/users/?user_id=${userId}`
-	)
+  try {
+    const response = await fetch(`${API_BASE_URL}/users/${userId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-	if (!response.ok) {
-		throw new Error(
-			`Ошибка при получении данных пользователя: ${response.status}`
-		)
-	}
+    if (!response.ok) {
+      throw new Error(`Ошибка HTTP: ${response.status}`);
+    }
 
-	const data: ITelegramUser = await response.json()
-
-	if (Array.isArray(data) && data.length > 0) {
-		return data
-	}
-
-	throw new Error('Пользователь не найден')
-}
+    const data: ITelegramUser[] = await response.json();
+    return data[0];
+  } catch (error) {
+    console.error("Ошибка при запросе к серверу:", error);
+    throw error;
+  }
+};
