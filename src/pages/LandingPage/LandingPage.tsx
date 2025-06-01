70fadffae03f147e3a46c6aea267693e572cb4fa
KoUD2 <kostik-udod@mail.ru>
2025-06-01 16:13:56 +0300

    Refactor LandingPage component and styles: update layout and design elements for improved aesthetics; replace logo images with a new asset; adjust text content and styles for better clarity and user engagement.

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,113 +1,57 @@
-import cn from 'classnames'
-import { FC } from 'react'
-import { Link } from 'react-router-dom'
-import useTheme from 'src/shared/hooks/useTheme'
-import c from '../../shared/assets/landing/c.svg'
-import cLight from '../../shared/assets/landing/cLight.svg'
-import m from '../../shared/assets/landing/m.svg'
-import mLight from '../../shared/assets/landing/mLight.svg'
-import n from '../../shared/assets/landing/n.svg'
-import nLight from '../../shared/assets/landing/nLight.svg'
-import o from '../../shared/assets/landing/o.svg'
-import oLight from '../../shared/assets/landing/oLight.svg'
-import styles from './LandingPage.module.css'
+import { FC } from "react";
+import { Link } from "react-router-dom";
+import useTheme from "src/shared/hooks/useTheme";
+import LogoConnect from "src/shared/assets/landing/LogoConnect.svg";
+import styles from "./LandingPage.module.css";
 
 const LandingPage: FC = () => {
-	const { theme } = useTheme()
+  const { theme } = useTheme();
 
-	console.log(theme)
+  console.log(theme);
 
-	return (
-		<div className={styles['landing']}>
-			<div className={styles['landing__wrapper']}>
-				<div
-					className={cn(styles['landing__logo'], styles['landing__logo_top'])}
-				>
-					{theme === 'dark' ? (
-						<>
-							<img className={styles['landing__letter']} src={c} alt='C' />
-							<img className={styles['landing__letter']} src={o} alt='O' />
-						</>
-					) : theme === 'light' ? (
-						<>
-							<img
-								className={styles['landing__letter']}
-								src={cLight}
-								alt='CLight'
-							/>
-							<img
-								className={styles['landing__letter']}
-								src={oLight}
-								alt='OLight'
-							/>
-						</>
-					) : null}
-				</div>
-				<div className={styles['landing__content']}>
-					<h1 className={styles['landing__title']}>Common Course</h1>
-					<p className={styles['landing__subtitle']}>
-						Образовательный маркетплейс для студентов
-					</p>
-				</div>
+  return (
+    <div className={styles["landing"]}>
+      <div className={styles["landing__wrapper"]}>
+        <div className={styles["landing__content"]}>
+          <img src={LogoConnect} alt="Картинка логотипа" />
+          <h1 className={styles["landing__title"]}>Common Connect</h1>
+          <p className={styles["landing__subtitle"]}>
+            Контакты тех, кто поможет с учебными задачами
+          </p>
+        </div>
 
-				<p className={styles['landing__agreement']}>
-					Продолжая создание профиля, я принимаю{' '}
-					<Link
-						to='https://disk.yandex.ru/i/h6bWlwR6L5B8fg'
-						target='_blank'
-						onClick={event => {
-							event.preventDefault()
-							window.open('https://disk.yandex.ru/i/h6bWlwR6L5B8fg')
-						}}
-						className={styles['landing__agreement-link']}
-					>
-						пользовательское соглашение
-					</Link>{' '}
-					и{' '}
-					<Link
-						to='https://disk.yandex.ru/i/Il8aGfCCgzVbnw'
-						target='_blank'
-						onClick={event => {
-							event.preventDefault()
-							window.open('https://disk.yandex.ru/i/Il8aGfCCgzVbnw')
-						}}
-						className={styles['landing__agreement-link']}
-					>
-						политику конфиденциальности
-					</Link>
-				</p>
-				<Link to='/registration'>
-					<button className={styles['landing__button']}>Создать профиль</button>
-				</Link>
-				<div
-					className={cn(
-						styles['landing__logo'],
-						styles['landing__logo_bottom']
-					)}
-				>
-					{theme === 'dark' ? (
-						<>
-							<img className={styles['landing__letter']} src={m} alt='C' />
-							<img className={styles['landing__letter']} src={n} alt='O' />
-						</>
-					) : theme === 'light' ? (
-						<>
-							<img
-								className={styles['landing__letter']}
-								src={mLight}
-								alt='CLight'
-							/>
-							<img
-								className={styles['landing__letter']}
-								src={nLight}
-								alt='OLight'
-							/>
-						</>
-					) : null}
-				</div>
-			</div>
-		</div>
-	)
-}
+        <p className={styles["landing__agreement"]}>
+          Продолжая создание профиля, я принимаю{" "}
+          <Link
+            to="https://disk.yandex.ru/i/h6bWlwR6L5B8fg"
+            target="_blank"
+            onClick={(event) => {
+              event.preventDefault();
+              window.open("https://disk.yandex.ru/i/h6bWlwR6L5B8fg");
+            }}
+            className={styles["landing__agreement-link"]}
+          >
+            пользовательское соглашение
+          </Link>{" "}
+          и{" "}
+          <Link
+            to="https://disk.yandex.ru/i/Il8aGfCCgzVbnw"
+            target="_blank"
+            onClick={(event) => {
+              event.preventDefault();
+              window.open("https://disk.yandex.ru/i/Il8aGfCCgzVbnw");
+            }}
+            className={styles["landing__agreement-link"]}
+          >
+            политику конфиденциальности
+          </Link>
+        </p>
+        <Link to="/registration">
+          <button className={styles["landing__button"]}>Создать профиль</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default LandingPage
+export default LandingPage;
